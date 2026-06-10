374bae08efaedff1c5165dcd4e1e8354ebb48535
Freddy Juhel <ext.freddy.juhel@reseau.sncf.fr>
2026-06-10 16:38:37 +0200

    add application config file version property

--- ConsoleAppDemoappConfigJson/ApplicationConfig.cs
+++ ConsoleAppDemoappConfigJson/ApplicationConfig.cs
@@ -1,8 +1,12 @@
-﻿namespace ConsoleAppDemoappConfigJson
+﻿using System;
+
+namespace ConsoleAppDemoappConfigJson
 {
   public class ApplicationConfig
   {
     public string ApplicationName { get; set; } = "MonApplication";
+
+    public Version ApplicationConfigFileVersion = new Version("2.0.0.0");
 
     public string LogLevel { get; set; } = "Info";
 
